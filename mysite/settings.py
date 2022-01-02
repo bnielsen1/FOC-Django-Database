0e51ce4a14ad2cb9c0a689777f944db3dd5106f0
bnielsen1 <bnielsen1@ufl.edu>
2022-01-02 15:54:03 -0500

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$6x!peh)i9gfq#&*6%+o%1l8ef2q4q3g$hnofs7qg+))vi**d@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "192.168.1.125"]
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
